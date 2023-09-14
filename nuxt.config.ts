abea525e7035a720f73e2dfbd560dbaef93df47c
Руслан Бельченков <ruslanbelcenkov@MacBook-Pro-Ruslan.local>
2023-09-14 11:38:59 +0300

    Added compression

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,6 +43,9 @@ export default defineNuxtConfig({
         '@nuxt/image',
         '@nuxtjs/strapi',
         'nuxt-delay-hydration',
+        ['@nuxt-modules/compression', {
+            algorithm: 'brotliCompress'
+        }]
     ],
     plugins: [
         '~/plugins/mitt.ts',
