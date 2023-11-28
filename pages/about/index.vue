1eff6684bdfaaa9e0f50c21def345138bd2a43c4
ruslanbelcenkov <ruslan.belchenkov@yandex.ru>
2023-11-29 01:18:40 +0300

    Changes

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -11,7 +11,8 @@
           <v-card-title tag="h1" class="text-uppercase font-size-36 px-0 text-pre-wrap" style="line-height: 40px">
             {{ page.title }}
           </v-card-title>
-          <v-card-subtitle tag="h4" class="text-uppercase font-size-18 px-0 text-pre-wrap mt-5" style="color: #333; line-height: 22px">
+          <v-card-subtitle tag="h4" class="text-uppercase font-size-18 px-0 text-pre-wrap mt-5"
+                           style="color: #333; line-height: 22px">
             {{ page.subtitle }}
           </v-card-subtitle>
           <v-card-text class="px-0">
@@ -70,34 +71,45 @@
           </template>
           <template v-else>
             <v-card-text>
-            <vueper-slides
-                class="no-shadow"
-                fixed-height="750px"
-                :visible-slides=" mobile ? 1 : 3"
-                :slide-ratio="1 / 4"
-                :arrows-outside="!mobile"
-                :bullets="doctors.length > 3"
-                :gap="8"
-                :dragging-distance="70">
-              <template #arrow-left>
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="27" viewBox="0 0 13 27" fill="none">
-                  <path d="M11 2L1 13.5L11 25" stroke="black" stroke-width="2" stroke-linecap="square"
-                        stroke-linejoin="round"/>
-                </svg>
+              <template v-if="doctors.length > 3">
+                <vueper-slides
+                    class="no-shadow"
+                    fixed-height="750px"
+                    :visible-slides=" mobile ? 1 : 3"
+                    :slide-ratio="1 / 4"
+                    :arrows-outside="!mobile"
+                    :bullets="doctors.length > 3"
+                    :gap="8"
+                    :dragging-distance="70">
+                  <template #arrow-left>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="27" viewBox="0 0 13 27" fill="none">
+                      <path d="M11 2L1 13.5L11 25" stroke="black" stroke-width="2" stroke-linecap="square"
+                            stroke-linejoin="round"/>
+                    </svg>
+                  </template>
+                  <vueper-slide v-for="(doctor, index) of doctors" :key="index">
+                    <template #content>
+                      <doctor-card-component :doctor="doctor"/>
+                    </template>
+                  </vueper-slide>
+                  <template #arrow-right>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="13" height="27" viewBox="0 0 13 27" fill="none">
+                      <path d="M2 25L12 13.5L2 2" stroke="black" stroke-width="2" stroke-linecap="square"
+                            stroke-linejoin="round"/>
+                    </svg>
+                  </template>
+                </vueper-slides>
               </template>
-              <vueper-slide v-for="(doctor, index) of doctors" :key="index">
-                <template #content>
-                  <doctor-card-component :doctor="doctor"/>
-                </template>
-              </vueper-slide>
-              <template #arrow-right>
-                <svg xmlns="http://www.w3.org/2000/svg" width="13" height="27" viewBox="0 0 13 27" fill="none">
-                  <path d="M2 25L12 13.5L2 2" stroke="black" stroke-width="2" stroke-linecap="square"
-                        stroke-linejoin="round"/>
-                </svg>
+              <template v-else>
+                <div class="d-flex align-baseline justify-start flex-column-gap-35">
+                  <template v-for="(doctor, index) of doctors" :key="index">
+                    <div style="flex: 0 0 auto; width: calc(100% / 4)">
+                      <doctor-card-component :doctor="doctor"/>
+                    </div>
+                  </template>
+                </div>
               </template>
-            </vueper-slides>
-          </v-card-text>
+            </v-card-text>
           </template>
 
         </v-card>
@@ -112,11 +124,12 @@ import 'vueperslides/dist/vueperslides.css'
 import {useCommonStore} from "~/store/common";
 import DoctorCardComponent from "~/components/DoctorCardComponent.vue";
 import CertificateCardComponent from "~/components/CertificateCardComponent.vue";
-import {ref, watch, onBeforeMount} from "vue"
+import {onBeforeMount, ref, watch} from "vue"
 import Doctor from "~/models/Doctor";
 import {useI18n} from "vue-i18n";
 import {useLangStore} from "~/store/lang";
 import {useDisplay} from "vuetify";
+
 definePageMeta({
   breadcrumb: 'pages.about.title'
 })
@@ -126,8 +139,8 @@ defineProps({
     default: 0
   }
 })
-const { locale, t } = useI18n()
-const { mobile } = useDisplay()
+const {locale, t} = useI18n()
+const {mobile} = useDisplay()
 
 definePageMeta({
   breadcrumb: 'pages.about.title'
@@ -140,7 +153,7 @@ const timeout = ref(true)
 const {find} = useStrapi()
 const commonStore = useCommonStore()
 
-onBeforeMount(async() => {
+onBeforeMount(async () => {
   await find('about', {
     populate: {
       image: {
