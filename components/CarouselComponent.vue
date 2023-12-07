6bdd156a6934f681480c379d9767b1de8c9cb96c
ruslanbelcenkov <ruslan.belchenkov@yandex.ru>
2023-12-07 12:44:20 +0300

    Changes

--- components/CarouselComponent.vue
+++ components/CarouselComponent.vue
@@ -30,78 +30,81 @@
         <chevron-right/>
       </v-btn>
     </template>
-    <div ref="delimitersContainer"
-         class="delimiters__container">
-      <template v-for="(delimiter, index) of slides.length" :key="delimiter">
-        <v-btn @click="current = index" icon variant="plain" style="opacity: 1" class="delimiter_button">
-          <svg-icon :style="getColor(index)" size="18" :path="mdiCircle"
-                    type="mdi"/>
-        </v-btn>
-      </template>
-    </div>
+    <template v-if="slides.length > 1">
+      <div ref="delimitersContainer"
+           class="delimiters__container">
+        <template v-for="(delimiter, index) of slides.length" :key="delimiter">
+          <v-btn @click="current = index" icon variant="plain" style="opacity: 1" class="delimiter_button">
+            <svg-icon :style="getColor(index)" size="18" :path="mdiCircle"
+                      type="mdi"/>
+          </v-btn>
+        </template>
+      </div>
+    </template>
     <v-carousel-item
         v-for="(slide, i) in slides"
         :key="i"
     >
       <div class="w-100 fill-height position-relative"
-           :class="{'d-flex align-center': mobile}"
-           :style="`background-color: ${slide.background}; background-image: url('https://dashboard.asvtrade.lt${slide.src}'); background-size: ${mobile ? 'cover' : 'contain'}; background-repeat: no-repeat`">
-        <template v-if="mobile">
-          <v-sheet
-              class="z-index-3 position-relative px-3"
-              color="transparent"
-              height="100%"
-          >
-            <lazy-client-only>
-              <div class="d-flex flex-column fill-height justify-center align-start"
-                   style="row-gap: 40px">
-                <template v-if="slide.textColor">
-                <h2 class="font-size-32" :style="$display.fontSize(display.height.value, 64) + `; color: ${slide.textColor ?? '#fff'}`" v-html="slide.title"></h2>
-                <p class="font-size-16" :style="`color: ${slide.textColor ?? '#fff'}`" v-html="slide.description"></p>
-                </template>
-                <template v-else>
-                  <h2 class="text-white font-size-32" :style="$display.fontSize(display.height.value, 64)" v-html="slide.title"></h2>
-                  <p class="text-white font-size-16" v-html="slide.description"></p>
-                </template>
-                <template v-if="slide.link">
-                  <v-btn  rounded="0" :ripple="false" variant="tonal" :href="slide.link"
-                          :style="`color: ${slide.buttonTextColor}; background-color: ${slide.buttonColor}`"
-                          class="more_info__button">
-                    {{ $t('buttons.details') }}
-                  </v-btn>
-                </template>
-              </div>
-            </lazy-client-only>
-          </v-sheet>
-        </template>
-        <template v-else>
-          <v-sheet
-              class="z-index-3 position-relative"
-              color="transparent"
-              :width="$display.slideContentWidth(display.width.value, 540)"
-              style="margin-left: calc((255 * 100%) / 1920)"
-              height="100%"
-          >
-            <lazy-client-only>
-              <div class="d-flex flex-column fill-height justify-center align-start"
-                   style="row-gap: 40px">
-                <template v-if="slide.textColor">
-                  <h2 :style="$display.fontSize(display.height.value, 64) + `; color: ${slide.textColor}`" v-html="slide.title"></h2>
-                  <p class="font-size-16" :style="`color: ${slide.textColor}`" v-html="slide.description"></p>
-                </template>
-                <template v-else>
-                  <h2 class="text-white" :style="$display.fontSize(display.height.value, 64)" v-html="slide.title"></h2>
-                  <p class="text-white font-size-16" v-html="slide.description"></p>
-                </template>
-                <template v-if="slide.link">
-                  <v-btn  rounded="0" :ripple="false" variant="outlined" :href="slide.link" class="more_info__button" :style="`color: ${slide.buttonTextColor}; background-color: ${slide.buttonColor}`">
-                    {{ $t('buttons.details') }}
-                  </v-btn>
-                </template>
-              </div>
-            </lazy-client-only>
-          </v-sheet>
-        </template>
+           :class="{'d-flex align-center': mobile}">
+        <v-img :src="`https://dashboard.asvtrade.lt${slide.src}`">
+          <template v-if="mobile">
+            <v-sheet
+                class="z-index-3 position-relative px-3"
+                color="transparent"
+                height="100%"
+            >
+              <lazy-client-only>
+                <div class="d-flex flex-column fill-height justify-center align-start"
+                     style="row-gap: 40px">
+                  <template v-if="slide.textColor">
+                    <h2 class="font-size-32" :style="$display.fontSize(display.height.value, 64) + `; color: ${slide.textColor ?? '#fff'}`" v-html="slide.title"></h2>
+                    <p class="font-size-16" :style="`color: ${slide.textColor ?? '#fff'}`" v-html="slide.description"></p>
+                  </template>
+                  <template v-else>
+                    <h2 class="text-white font-size-32" :style="$display.fontSize(display.height.value, 64)" v-html="slide.title"></h2>
+                    <p class="text-white font-size-16" v-html="slide.description"></p>
+                  </template>
+                  <template v-if="slide.link">
+                    <v-btn  rounded="0" :ripple="false" variant="tonal" :href="slide.link"
+                            :style="`color: ${slide.buttonTextColor}; background-color: ${slide.buttonColor}`"
+                            class="more_info__button">
+                      {{ $t('buttons.details') }}
+                    </v-btn>
+                  </template>
+                </div>
+              </lazy-client-only>
+            </v-sheet>
+          </template>
+          <template v-else>
+            <v-sheet
+                class="z-index-3 position-relative"
+                color="transparent"
+                :width="$display.slideContentWidth(display.width.value, 540)"
+                style="margin-left: calc((255 * 100%) / 1920)"
+                height="100%"
+            >
+              <lazy-client-only>
+                <div class="d-flex flex-column fill-height justify-center align-start"
+                     style="row-gap: 40px">
+                  <template v-if="slide.textColor">
+                    <h2 :style="$display.fontSize(display.height.value, 64) + `; color: ${slide.textColor}`" v-html="slide.title"></h2>
+                    <p class="font-size-16" :style="`color: ${slide.textColor}`" v-html="slide.description"></p>
+                  </template>
+                  <template v-else>
+                    <h2 class="text-white" :style="$display.fontSize(display.height.value, 64)" v-html="slide.title"></h2>
+                    <p class="text-white font-size-16" v-html="slide.description"></p>
+                  </template>
+                  <template v-if="slide.link">
+                    <v-btn  rounded="0" :ripple="false" variant="outlined" :href="slide.link" class="more_info__button" :style="`color: ${slide.buttonTextColor}; background-color: ${slide.buttonColor}`">
+                      {{ $t('buttons.details') }}
+                    </v-btn>
+                  </template>
+                </div>
+              </lazy-client-only>
+            </v-sheet>
+          </template>
+        </v-img>
       </div>
 
     </v-carousel-item>
@@ -176,8 +179,12 @@ watch(current, (value) => {
   align-items: center;
   width: 150px !important;
   height: 60px !important;
-  color: #fff;
   font-size: 18px;
   font-weight: 600;
+  @media (max-width: 920px) {
+    width: 120px !important;
+    height: 45px !important;
+    font-size: 16px;
+  }
 }
 </style>
