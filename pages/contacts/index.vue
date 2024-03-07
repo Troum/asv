3181f7db8fa8642f65e7c15b68abedb3b49d5671
ruslanbelchenkov <troum@outlook.com>
2024-03-07 19:12:46 +0300

    Update

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -60,24 +60,7 @@
           </v-card>
         </div>
         <div ref="map" style="filter: grayscale(100%)">
-          <template v-if="locale === 'ru'">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d1352.7673177295308!2d25.260600502786573!3d54.606350913179725!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTTCsDM2JzIyLjgiTiAyNcKwMTUnNDAuOCJF!5e0!3m2!1sru!2sby!4v1701363302018!5m2!1sru!2sby"
-                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </template>
-          <template v-if="locale === 'lt'">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d2310.866131303371!2d25.2606005!3d54.6063509!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTTCsDM2JzIyLjgiTiAyNcKwMTUnNDAuOCJF!5e0!3m2!1slt!2sby!4v1701363419281!5m2!1slt!2sby"
-                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </template>
-          <template v-if="locale === 'en'">
-            <iframe
-                src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d2310.866131303371!2d25.2606005!3d54.6063509!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNTTCsDM2JzIyLjgiTiAyNcKwMTUnNDAuOCJF!5e0!3m2!1sen!2sby!4v1701363488626!5m2!1sen!2sby"
-                width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
-          </template>
+          <MapComponent />
         </div>
       </div>
     </v-col>
